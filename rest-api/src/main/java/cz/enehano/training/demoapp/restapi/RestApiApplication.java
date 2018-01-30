f1be07781f58b5340a83fd657b0f345d843eab30
Petr Vích <vichpetr@gmail.com>
2018-01-30 21:36:12 +0100

    create new rest api project

---
+++ rest-api/src/main/java/cz/enehano/training/demoapp/restapi/RestApiApplication.java
@@ -0,0 +1,12 @@
+package cz.enehano.training.demoapp.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+}
